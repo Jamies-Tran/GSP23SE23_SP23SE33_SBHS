bf21696e56539a47be61b9252e7f1106d0a1bec6
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-01-21 09:24:37 +0700

    Authen Login Register

--- SBHS_FE/src/app/app-routing.module.ts
+++ SBHS_FE/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginAdminComponent } from './admin/login-admin/login-admin.component';
 import { LoginPasssengerComponent } from './passenger/login-passsenger/login-passsenger.component';
 import { LoginLandlordComponent } from './landlord/login-landlord/login-landlord.component';
+import { RegisterComponent } from './landlord/register/register.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 const routes: Routes = [
   { path: 'Admin', component: LoginAdminComponent },
   { path: 'Passenger', component: LoginPasssengerComponent },
   { path: 'Landlord', component: LoginLandlordComponent },
+  { path: 'Register' , component: RegisterComponent},
+  { path: 'ForgotPassword' , component: ForgotPassComponent},
 ];
 
 @NgModule({
--- SBHS_FE/src/app/app.module.ts
+++ SBHS_FE/src/app/app.module.ts
@@ -6,7 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 // Component
 import { AppComponent } from './app.component';
@@ -14,15 +18,18 @@ import { LoginAdminComponent } from './admin/login-admin/login-admin.component';
 import { LoginLandlordComponent } from './landlord/login-landlord/login-landlord.component';
 import { LoginPasssengerComponent } from './passenger/login-passsenger/login-passsenger.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { RegisterComponent } from './landlord/register/register.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginAdminComponent,
     LoginLandlordComponent,
-    LoginPasssengerComponent
+    LoginPasssengerComponent,
+    RegisterComponent,
+    ForgotPassComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,10 +39,14 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
