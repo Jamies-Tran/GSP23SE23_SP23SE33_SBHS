82c671d951791130b897d99c8a71cf1f6fb0db1d
JamiesTran <minhtranquang2197@gmail.com>
2023-02-06 07:41:02 +0700

    Add cors configuration

---
+++ swm/src/main/java/com/sbhs/swm/configuration/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.sbhs.swm.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST",
+                        "PUT", "DELETE").allowedHeaders("*");
+                WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+        };
+    }
+}
