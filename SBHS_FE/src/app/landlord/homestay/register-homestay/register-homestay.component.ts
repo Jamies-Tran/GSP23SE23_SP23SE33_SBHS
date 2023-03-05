d50e5a8ada4a3aeb6032c12c82e36c44e9cf0044
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-03-05 19:27:54 +0700

    fix homestay register

--- SBHS_FE/src/app/landlord/homestay/register-homestay/register-homestay.component.ts
+++ SBHS_FE/src/app/landlord/homestay/register-homestay/register-homestay.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatStepper } from '@angular/material/stepper';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ServerHttpService } from 'src/app/services/homestay.service';
 
 @Component({
   selector: 'app-register-homestay',
@@ -11,8 +12,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class RegisterHomestayComponent implements OnInit {
   //
-  constructor(private _formBuilder: FormBuilder,  public dialog: MatDialog,private router: Router,
-    private route: ActivatedRoute) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: ServerHttpService
+  ) {}
 
   // homestayName: string = '';
   // totalRoom: string = '';
@@ -23,48 +29,50 @@ export class RegisterHomestayComponent implements OnInit {
 
   ngOnInit(): void {}
 
-
   // information
 
   informationFormGroup = this._formBuilder.group({
     homestayName: ['', Validators.required],
     address: ['', Validators.required],
     number: ['', Validators.required],
-
+    homestayLicense: [false, Validators.requiredTrue],
   });
-  homestayName : string ="";
-  address : string ="";
-  totalRoom : string ="";
+  homestayName: string = '';
+  address: string = '';
+  totalRoom: string = '';
   informationForm() {
     // Lay value
-
+    console.log(this.informationFormGroup.value);
     const formInformationFormGroupValue = this.informationFormGroup.controls;
     this.homestayName = formInformationFormGroupValue.homestayName.value!;
     this.address = formInformationFormGroupValue.address.value!;
     this.totalRoom = formInformationFormGroupValue.number.value!;
-    localStorage.setItem("homestayName",this.homestayName);
-    localStorage.setItem("address",this.address);
-    localStorage.setItem("totalRoom",this.totalRoom);
+    localStorage.setItem('homestayName', this.homestayName);
+    localStorage.setItem('address', this.address);
+    localStorage.setItem('totalRoom', this.totalRoom);
   }
 
   // Facility
   facilityFormGroup = this._formBuilder.group({
     tv: false,
-    tvAmount:[{ value: '', disabled: true }],
+    tvAmount: [{ value: '', disabled: true }],
     wifi: false,
-    wifiAmount:[{ value: '', disabled: true }],
+    wifiAmount: [{ value: '', disabled: true }],
   });
-  homestayFacilities: Array<{name:string,quantity:string}> = [];
+  homestayFacilities: Array<{ name: string; quantity: string }> = [];
   facilityForm() {
     console.log(' this.newFacility.push', this.newFacility);
     console.log(this.facilityFormGroup.value);
-    if(this.facilityFormGroup.value["tv"] === true){
-      this.homestayFacilities.push({name:"tv",quantity:"2"})
+    if (this.facilityFormGroup.value['tv'] === true) {
+      this.homestayFacilities.push({ name: 'tv', quantity: '2' });
     }
-    if(this.facilityFormGroup.value["wifi"] === true){
-      this.homestayFacilities.push({name:"wifi",quantity:"2"})
+    if (this.facilityFormGroup.value['wifi'] === true) {
+      this.homestayFacilities.push({ name: 'wifi', quantity: '2' });
     }
-    localStorage.setItem("homestayFacilities",JSON.stringify(this.homestayFacilities))
+    localStorage.setItem(
+      'homestayFacilities',
+      JSON.stringify(this.homestayFacilities)
+    );
   }
   enableInputTv() {
     if (this.facilityFormGroup.controls.tv.value === true) {
@@ -116,19 +124,20 @@ export class RegisterHomestayComponent implements OnInit {
     breakfastPrice: [{ value: '', disabled: true }],
     swimming: false,
     swimmingPrice: [{ value: '', disabled: true }],
-  })
+  });
 
-  serviceForm(){
+  serviceForm() {
     console.log(this.serviceFormGroup.value);
     console.log(this.newService);
-    let homestayServices : any =[];
-    if(this.serviceFormGroup.value["breakfast"] === true){
+    let homestayServices: any = [];
+    if (this.serviceFormGroup.value['breakfast'] === true) {
       // homestayServices.push({name: "Breakfast",price: this.serviceFormGroup.value["breakfastPrice"]["value"]})
-      console.log(this.serviceFormGroup.value["breakfastPrice"])
+      console.log(this.serviceFormGroup.value['breakfastPrice']);
     }
     this.router.navigate(['/Landlord/Homestay/Category/Overview'], {
-      relativeTo: this.route})
-}
+      relativeTo: this.route,
+    });
+  }
 
   enableInputBreakfast() {
     if (this.serviceFormGroup.controls.breakfast.value === true) {
@@ -150,7 +159,7 @@ export class RegisterHomestayComponent implements OnInit {
   newService: any[] = [];
 
   addService() {
-    this.newService.push({ name: '',price: '', status: false });
+    this.newService.push({ name: '', price: '', status: false });
     console.log('values', this.newService);
     console.log('size', this.newService.length);
   }
@@ -161,13 +170,88 @@ export class RegisterHomestayComponent implements OnInit {
     console.log('delete', this.newService.length + i);
   }
 
-  step1(){
-    this.stepper.selectedIndex = 1;
+  step1() {
+    this.stepper.selectedIndex = 0;
   }
-  step3(){
-    this.stepper.selectedIndex = 3;
-  }
-  step4(){
+  step5() {
     this.stepper.selectedIndex = 4;
   }
+  step6() {
+    this.stepper.selectedIndex = 5;
+  }
+
+  // Register Image
+  file!: File;
+  homestayLicenseFiles: File[] = [];
+  homestayImageFiles: File[] = [];
+
+  public homestayLicense!: string;
+  public homestayImages: string[] = [];
+
+  // lấy file hình
+  onSelectHomestayLicense(files: any) {
+    console.log('onselect: ', files);
+    // set files
+    this.homestayLicenseFiles.push(...files.addedFiles);
+    console.log('file array', this.homestayLicenseFiles);
+    console.log('file lenght', this.homestayLicenseFiles.length);
+
+    if (this.homestayLicenseFiles.length > 1) {
+      this.homestayLicenseFiles.splice(
+        this.homestayLicenseFiles.indexOf(files),
+        1
+      );
+      console.log('file lenght slice', this.homestayLicenseFiles.length);
+      console.log('file array', this.homestayLicenseFiles);
+      console.log('file index', this.homestayLicenseFiles.indexOf(files));
+    }
+    if (
+      this.homestayImageFiles.length >= 1 &&
+      this.homestayLicenseFiles.length == 1
+    ) {
+      this.informationFormGroup.patchValue({ homestayLicense: true });
+    } else {
+      this.informationFormGroup.patchValue({ homestayLicense: false });
+    }
+  }
+
+  // xóa file hình
+  onRemoveHomestayLicense(event: File) {
+    console.log(event);
+    console.log('xoa index:', this.homestayLicenseFiles.indexOf(event));
+    this.homestayLicenseFiles.splice(
+      this.homestayLicenseFiles.indexOf(event),
+      1
+    );
+    console.log('xoa file:', this.homestayLicenseFiles);
+    if (
+      this.homestayImageFiles.length >= 1 &&
+      this.homestayLicenseFiles.length == 1
+    ) {
+      this.informationFormGroup.patchValue({ homestayLicense: true });
+    } else {
+      this.informationFormGroup.patchValue({ homestayLicense: false });
+    }
+  }
+
+  // lấy file hình
+  onSelectImageHomestay(files: any) {
+    console.log('onselect: ', files);
+    // set files
+    this.homestayImageFiles.push(...files.addedFiles);
+  }
+
+  // xóa file hình
+  onRemoveHomestayImage(event: File) {
+    console.log(event);
+    this.homestayImageFiles.splice(this.homestayImageFiles.indexOf(event), 1);
+    console.log('xoa file:', this.homestayImageFiles);
+  }
+
+  // Price
+  public price = '';
+
+  // register submit
+  result: string = '';
+  register() {}
 }
