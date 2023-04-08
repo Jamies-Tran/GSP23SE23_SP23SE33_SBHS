0fa9cd51106b2bef762448fa35d8af5250b7336b
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-04-08 22:44:32 +0700

    accept reject booking homestay

---
+++ SBHS_FE/src/app/pop-up/booking-pending/booking-pending.component.ts
@@ -0,0 +1,53 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { BookingService } from 'src/app/services/booking.service';
+import { MessageComponent } from '../message/message.component';
+import { SuccessComponent } from '../success/success.component';
+
+@Component({
+  selector: 'app-booking-pending',
+  templateUrl: './booking-pending.component.html',
+  styleUrls: ['./booking-pending.component.scss']
+})
+export class BookingPendingComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {  bookingId:number; homestayId:number; },
+    public dialog: MatDialog,
+    private http: BookingService
+  ) {}
+  reason !: string;
+  message: any;
+  openDialogMessage() {
+    localStorage.setItem('action-pending', 'true');
+    this.dialog.open(MessageComponent, {
+      data: this.message,
+    });
+  }
+  openDialogSuccess() {
+    this.dialog.open(SuccessComponent, {
+      data: this.message,
+    });
+  }
+
+  reject(){
+    try {
+      this.http.rejectBookingForHomestay(this.data.bookingId , this.data.homestayId, this.reason).subscribe({
+        next:(data:any) =>{
+          this.message = "Reject Booking Homestay Success";
+          this.openDialogSuccess();
+
+        },
+        error: (error)=>{
+          this.message= error;
+          this.openDialogMessage();
+        }
+      }
+      )
+    } catch (error) {
+      this.message = error as string;
+      this.openDialogMessage();
+      console.log(error);
+    }
+  }
+}
