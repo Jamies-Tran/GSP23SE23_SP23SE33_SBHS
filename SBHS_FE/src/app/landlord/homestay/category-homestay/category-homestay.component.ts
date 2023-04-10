d9b620fe033efe166847cdddee5a84eac0f22a09
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-04-10 10:38:10 +0700

    up domain

--- SBHS_FE/src/app/landlord/homestay/category-homestay/category-homestay.component.ts
+++ SBHS_FE/src/app/landlord/homestay/category-homestay/category-homestay.component.ts
@@ -3,7 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category-homestay',
   templateUrl: './category-homestay.component.html',
-  styleUrls: ['./category-homestay.component.scss']
+  styles: [`
+  .card-body{
+
+    .body-content{
+      width: 270px;
+      height: 175px;
+      overflow: hidden;
+    }
+    .image{
+      min-width: 280px;
+      min-height: 170px;
+
+    }
+    .image-bloc{
+      img{
+        width: 190px;
+      }
+    }
+  }
+
+  .button-choose{
+    button{
+      background-color: #03A9F4;
+      color: white;
+
+    }
+
+  }
+  `]
 })
 export class CategoryHomestayComponent {
 
